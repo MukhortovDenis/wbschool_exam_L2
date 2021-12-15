135e62669fe5ce27d9f01e47e86b5a2166439408
Denis Muhortov <muhortovdenis48@gmail.com>
2021-12-15 18:31:00 +0300

    задача 8

--- develop/dev08/task.go
+++ develop/dev08/task.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"os/exec"
+	"strings"
+)
+
 /*
 === Взаимодействие с ОС ===
 
@@ -14,6 +25,89 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+func shell(str string) error {
+	var cmd *exec.Cmd
+	str = strings.TrimSuffix(str, "\n")
+	slice := strings.Fields(str)
+	switch slice[0] {
+	case "cd":
+		if len(slice) != 2 {
+			return errors.New("неправильный путь")
+		}
+		return os.Chdir(slice[1])
+	case "pwd":
+		if len(slice) != 1 {
+			return errors.New("лишние аргументы")
+		}
+		path, _ := os.Getwd()
+		fmt.Println(path)
+		return nil
+	case "echo":
+		s := strings.Join(slice[1:], " ")
+		_, err := io.WriteString(os.Stdout, s)
+		if err != nil {
+			return err
+		}
+		return nil
+	case "ps":
+		cmd = exec.Command("powershell", "ps")
+		cmd.Stderr = os.Stderr
+		cmd.Stdout = os.Stdout
+		return cmd.Run()
+	case "kill":
+		cmd = exec.Command("powershell", "kill", slice[1])
+		cmd.Stderr = os.Stderr
+		cmd.Stdout = os.Stdout
+		return cmd.Run()
+	case "netcat":
+		return netcat(fmt.Sprintf("%s:%s", slice[1], slice[2]))
+	case "exit":
+		os.Exit(0)
+	}
+	if !strings.Contains(str, "|") {
+		cmd = exec.Command(slice[0], slice[1:]...)
+	} else {
+		cmd = exec.Command("powershell", `.\` + str)
+	}
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	return cmd.Run()
+}
 
+func netcat(addr string) error {
+	var conn net.Conn
+	var err error
+	conn, err = net.Dial("tcp", addr)
+	if err != nil {
+		conn, err = net.Dial("udp", addr)
+		if err != nil {
+			fmt.Printf("Can't connect to server: %s\n", err)
+			return err
+		}
+	}
+	_, err = io.Copy(conn, os.Stdin)
+	if err != nil {
+		fmt.Printf("Connection error: %s\n", err)
+		return err
+	}
+	return nil
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		dir, err := os.Getwd()
+		if err != nil {
+			_, _ = fmt.Fprintln(os.Stderr, err)
+		}
+		fmt.Printf("%v>", dir)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			_, _ = fmt.Fprintln(os.Stderr, err)
+		}
+		err = shell(input)
+		if err != nil {
+			_, _ = fmt.Fprintln(os.Stderr, err)
+		}
+	}
 }
