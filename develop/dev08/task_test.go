9eb529e644190d9eaa4f9dab3727e812c602bcc8
Denis Muhortov <muhortovdenis48@gmail.com>
2021-12-16 14:09:05 +0300

    задача 9

--- develop/dev08/task_test.go
+++ develop/dev08/task_test.go
@@ -1,1 +1,17 @@
 package main
+
+import "testing"
+
+
+func Test_shell(t *testing.T){
+	slice := []string{
+		"cd ..", "pwd", "echo test check", "ps"}
+	t.Run("проверка шелла", func(t *testing.T){
+		for _, str := range slice{
+			err := shell(str)
+			if err != nil{
+				t.Error(err)
+			}
+		}
+	})
+}
